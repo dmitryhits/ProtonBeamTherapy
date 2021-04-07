f9a6b71853279968a754080f3ca6cebf4d0a5a04
dmitryhits <dmitry.hits@gmail.com>
2021-04-07 15:17:54 +0200

    change core to macro_tools, starts scanner_tools

--- gate_simulation/_nbdev.py
+++ gate_simulation/_nbdev.py
@@ -2,13 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"Ek": "00_core.ipynb",
-         "create_sensor": "00_core.ipynb",
-         "create_phantom_layer": "00_core.ipynb",
-         "create_beam": "00_core.ipynb",
-         "create_start_beams": "00_core.ipynb",
-         "create_macro_file": "00_core.ipynb",
-         "run_macro": "00_core.ipynb"}
+index = {"Ek": "00_macro_tools.ipynb",
+         "MacroWriter": "00_macro_tools.ipynb",
+         "run_macro": "00_macro_tools.ipynb"}
 
 modules = ["core.py"]
 
